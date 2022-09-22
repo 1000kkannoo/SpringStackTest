0a13865d275fd9e0abe19df916be8c1239dcdda3
Hyun Woo <hwsa10041@gmail.com>
2022-09-22 15:17:29 +0900

    프로젝트 세팅

---
+++ src/main/java/me/SpringStackTest/SpringStackTestApplication.java
@@ -0,0 +1,13 @@
+package me.SpringStackTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStackTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStackTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/SpringStackTest/SpringStackTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.SpringStackTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStackTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
